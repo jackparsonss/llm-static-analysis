e1e32de24efe45a7bcb1c660aa10f109fa58e59c
Jack Parsons <jparsons@ualberta.ca>
2023-11-21 12:52:36 -0700

    More filtering

--- src/data.py
+++ src/data.py
@@ -1,5 +1,9 @@
 # datset: https://huggingface.co/datasets/thepurpleowl/codequeries
 import datasets
+from collections import defaultdict
+
+counter = defaultdict(int)
+seen = set()
 
 valid_queries = [
     "Unnecessary pass",
@@ -15,12 +19,41 @@ valid_queries = [
 ]
 
 
-def filter_data():
-    ds = datasets.load_dataset(
-        "thepurpleowl/codequeries", "twostep", split=datasets.Split.TRAIN
-    )
+def filter(row):
+    name = row["query_name"]
+    is_valid = name in valid_queries
 
-    ds = ds.filter(lambda row: row["query_name"] in valid_queries)
+    if not is_valid:
+        return False
+
+    # remove dups
+    pair = (name, row["code_file_path"])
+    if pair in seen:
+        return False
+
+    # limit to 5
+    counter[name] += 1
+    if counter[name] > 5:
+        return False
+
+    seen.add(pair)
+    return True
+
+
+def filter_data():
+    ds = datasets.load_dataset("thepurpleowl/codequeries", "twostep")
+    cols_to_remove = [
+        "context_block",
+        "answer_spans",
+        "supporting_fact_spans",
+        "example_type",
+        "single_hop",
+        "subtokenized_input_sequence",
+        "label_sequence",
+    ]
+    ds = ds.remove_columns(cols_to_remove)
+
+    ds = ds.filter(filter)
     ds.save_to_disk("filtered_data")
 
     return ds
